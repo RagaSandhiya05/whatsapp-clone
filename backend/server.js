8e30f313ff4112f867d1eabb10d07e0305cd30a0
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-03-30 20:20:17 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+require("dotenv").config();
 
+const connectDB = require("./config/db");
+
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/messages", messageRoutes);
+
+// SOCKET.IO
+io.on("connection", (socket) => {
+  socket.on("sendMessage", (data) => {
+    io.emit("receiveMessage", data);
+  });
+});
+
+server.listen(5000, () => console.log("Server running on port 5000"));
